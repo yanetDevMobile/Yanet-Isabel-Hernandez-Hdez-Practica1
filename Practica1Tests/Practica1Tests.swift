eb6b7e9b21328e3e0fece53da7b2e7e6f771fdd8
usuario <usuario@MacBook-Pro-de-07.local>
2019-01-07 12:33:21 -0600

    Initial Commit

---
+++ Practica1Tests/Practica1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Practica1Tests.swift
+//  Practica1Tests
+//
+//  Created by usuario on 1/7/19.
+//  Copyright © 2019 usuario. All rights reserved.
+//
+
+import XCTest
+@testable import Practica1
+
+class Practica1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
